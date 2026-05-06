b0ab2a45a8893f34f584435757ba10d8b65dd052
shadabsabiri <shadabsabiri01@gmail.com>
2026-05-07 00:49:44 +0530

    Connected mongodb atlas and created models

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,26 @@
 require("dotenv").config();
+const scrapeHackerNews = require("./utils/scraper");
 const app = require("./app");
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+
+const startServer = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected");
+
+    // await scrapeHackerNews();  //this pure logic for auto-run
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+startServer();
